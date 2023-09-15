7d2fb9e7c003c1f3793249edf850888b8b5b1819
ThomasFaria <thomas.faria@ensae.fr>
2023-09-15 14:10:12 +0000

    add duckdb

--- tp/tp-manip-donnees-massives.R
+++ tp/tp-manip-donnees-massives.R
@@ -1,6 +1,5 @@
 # mc cp s3/projet-formation/diffusion/bceao/data_recensement_2017.csv formation-bceao/tp/data/data_recensement_2017.csv
 
-
 library(ggplot2)
 library(dplyr)
 # Nous avons mis à votre disposition un jeu de données de 4Go provenant du recensement de la population. 
@@ -119,7 +118,7 @@ data <- arrow::read_parquet(
 # Un logement de de fortune est caractérisé par la modalité 5 de la variable TYPL
 t <- Sys.time()
 data |> 
-  filter(REGION == "76") |>
+  # filter(REGION == "76") |>
   select(REGION, COMMUNE, TYPL) |> 
   group_by(COMMUNE) |>
   summarise(
@@ -133,7 +132,7 @@ data |>
 elapsed_in_memory <- as.numeric(difftime(Sys.time(), t, units = "secs"))
 
 ############################################################
-# Manipulation des données parquet en mode "Lazy"
+# Manipulation des données parquet en évalutaion "Lazy"
 ############################################################
 
 # On importe le dataset en mode lazy
@@ -151,7 +150,7 @@ names(data_lazy)
 # On reproduit le même calcul que précedemment. Pour cela on définit dans un premier temps la requête.
 # L'execution est instantanée puisqu'aucun calcul n'a encore été réalisé
 query <- data_lazy |> 
-  filter(REGION == "76") |>
+  # filter(REGION == "76") |>
   select(REGION, COMMUNE, TYPL) |> 
   group_by(COMMUNE) |>
   summarise(
@@ -164,13 +163,78 @@ query <- data_lazy |>
   arrange(-part_logements_fortune)
 
 
-# Une fois la requête créée, il existe deux méthodes pour réaliser le calcul.
-# La première est la fonction collect()
+# Une fois la requête créée, il existe deux méthodes pour évaluer la requête (i.e. réaliser le calcul).
+
+# La première est la méthode compute()
+# Cette méthode execute la requête et renvoie les données au format d'objet Arrow
+query |> compute()
+
+# La seconde est la fonction collect()
+# Cette méthode execute la requête et renvoie les données au format tibble
+t <- Sys.time()
+query |> collect()
+elapsed_lazy <- as.numeric(difftime(Sys.time(), t, units = "secs"))
+
+
+############################################################
+# Manipulation depuis la base de données DuckDB
+############################################################
+
+con <- dbConnect(duckdb::duckdb(), dbdir="tp/data/data_recensement_2017.duckdb", read_only=TRUE)
+dbListTables(con)
+query_sql <- "SELECT
+                COMMUNE,
+                SUM(CASE WHEN TYPL = '5' THEN 1 ELSE 0 END) AS nb_logements_fortune,
+                COUNT(*) AS nb_logements_commune,
+                SUM(CASE WHEN TYPL = '5' THEN 1 ELSE 0 END) / COUNT(*) AS part_logements_fortune
+              FROM
+                dataset
+              GROUP BY
+                COMMUNE
+              ORDER BY
+                part_logements_fortune DESC;"
 
 t <- Sys.time()
+dbGetQuery(con, query_sql) |> as_tibble()
+elapsed_duckdb <- as.numeric(difftime(Sys.time(), t, units = "secs"))
+duckdb::dbDisconnect(con)
+
+############################################################
+# Evaluation de la performance
+############################################################
+
+performance <- dplyr::tibble(format_text = c("in memory", "arrow lazy", "duckDB"),
+                             elapse_time = round(c(elapsed_in_memory, elapsed_lazy, elapsed_duckdb),3))
 
 
-# la seconde est la méthode compute()
+performance |> 
+  dplyr::mutate(format_text = forcats::fct_reorder(format_text, elapse_time)) |> 
+  ggplot(aes(format_text, elapse_time))+
+  geom_col(fill = "lightblue", width=0.6)+
+  geom_text(aes(label = glue::glue("{elapse_time} sec")), hjust = 1, nudge_x = 0.4, size = 4, fontface = "bold") +
+  coord_flip()+
+  labs(title = "Performances des requêtes",
+       x = "",
+       y = "Temps de calcul en secondes")+
+  theme_light()
 
-t <- Sys.time()
+
+############################################################
+# Accès au données depuis le Cloud
+############################################################
+# mc cp -r formation-bceao/tp/data/data_partitioned/ s3/projet-formation/diffusion/bceao/data_partitioned/
+
+
+# On peut lister les fichiers présent dans le bucket
+aws.s3::get_bucket("projet-formation", prefix = "diffusion/bceao", region = "")
+
+# Lecture du dataset parquet directement depuis le cloud
+data_cloud <- arrow::open_dataset(
+  source = "tp/data/data_partitioned",
+  partitioning = arrow::schema(REGION = arrow::utf8()))
+
+
+
+
+
 
